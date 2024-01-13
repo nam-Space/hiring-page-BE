e5d57934631c8a8baad77d38be292dab3b2379cf
PTITer2003 <namd21ptit@gmail.com>
2024-01-13 10:15:40 +0700

    first commit

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Mật khẩu không được để trống',
+  })
+  password: string;
+  name: string;
+  phone: string;
+}
